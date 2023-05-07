fad970ca554708014d32b2ede96b0604ea1540a9
Minhan Tang <1900574002@qq.com>
2023-05-07 11:42:30 -0500

    Not a true sliding window though

--- MT_helpers/Sliding_Window.py
+++ MT_helpers/Sliding_Window.py
@@ -4,150 +4,78 @@ from matplotlib.pylab import plt
 
 class SlidingWindow:
     @staticmethod
-    def detect_behavior(sequence: str, fps=3, tolerance=1, detect_second=3, num_classes=5, priority=None) -> int:
-        if priority is None:
-            priority = [1] * num_classes
-        seq_len = len(sequence)
-        detected = [False] * num_classes
-
-        lp = 0
-        rp = min(lp + detect_second * fps + 1, len(sequence), seq_len)
-        occurrence = {}
-        for i in range(num_classes):
-            occurrence[str(i)] = 0
-        for i in range(lp, rp):
-            occurrence[sequence[i]] += 1
-            # it matters, and it has exceeded the requirements
-            if priority[int(sequence[i])] > 0 and occurrence[sequence[i]] >= detect_second * fps - tolerance:
-                detected[int(sequence[i])] = True
-
-        while rp < seq_len:
-            occurrence[sequence[rp]] += 1
-            # it matters, and it has exceeded the requirements
-            if priority[int(sequence[rp])] > 0 and occurrence[sequence[rp]] >= detect_second * fps - tolerance:
-                detected[int(sequence[rp])] = True
-
-            # delete the left and add the right of sliding window
-            occurrence[sequence[lp]] -= 1
-            lp += 1
-            rp += 1
-
-        detected_value = 0
-        max_priority = 0
-        for i in range(num_classes):
-            if detected[i] and priority[i] > max_priority:
-                max_priority = priority[i]
-                detected_value = i
-        return detected_value
-
-    @staticmethod
-    def switch_window(sequence: str, fps=3, tolerance=1, detect_second=3, num_classes=5, priority=None) -> int:
-        detected = [0] * num_classes  # store detection result, representing maximum consecutive frames
-        current_detection = [0] * num_classes
+    def switch_window(sequence: str, fps=3, window_size=1, detect_second=3, num_classes=5, tolerance=0) -> int:
         # window queue for detection
-        window = [int(seq) for seq in sequence[:min(max(tolerance - 1, 0), len(sequence) - 1)]]
-        # switch for window
-        window_switch = [False] * num_classes
+        window = [int(seq) for seq in sequence[:window_size]]
+        latent_occurrence = [0] * num_classes
+        latent_occurrence[max(set(window), key=window.count)] += 1
 
         # slide
-        for s in range(len(sequence) - tolerance):
+        for s in range(len(sequence) - window_size):
             window.append(int(sequence[s + len(window)]))
             window.pop(0)
-            window_set = set(window)
-            # check for switch
-            if len(window_set) == 1:
-                window_switch[window[0]] = True
-                current_detection[window[0]] = s
+            latent_occurrence[max(set(window), key=window.count)] += 1
 
-            for classes in range(num_classes):
-                if classes not in window_set and window_switch[classes]:
-                    window_switch[classes] = False
-                    detected[classes] = max(detected[classes], s - current_detection[classes] + len(window))
+        # output detection, 2 > 1
+        if latent_occurrence[2] >= fps * detect_second - tolerance:
+            return 2
+        max_occur = 0
+        max_num = 0
+        for i in [1, 3, 4]:
+            if latent_occurrence[i] >= max(fps * detect_second - tolerance, max_occur):
+                max_occur = latent_occurrence[i]
+                max_num = i
+        return max_num
 
-        # output the first successful detection with the highest hierarchy
-        max_priority = -1
-        detected_value = 0
-        for detector in range(len(detected)):
-            if detected[detector] >= fps * detect_second and priority[detector] > max_priority:
-                max_priority = priority[detector]
-                detected_value = detector
-        return detected_value
 
-    @staticmethod
-    def dataset_detection(path, frames=30, tolerant=20, priority=[0, 1, 2, 1, 1]):
-        correct_N_total = []
-        for filename in os.listdir(path):
-            tot_input = 0
-            correct_output = 0
-            file_path = os.path.join(path, filename)
-            actual_label = int(filename[0])
-            with open(file_path) as f_in:
-                for line in f_in:
-                    tot_input += 1
-                    detected_label = SlidingWindow.detect_behavior(line.strip(), fps=frames,
-                                                                   tolerance=tolerant, priority=priority)
-                    correct_output = correct_output + 1 if actual_label == detected_label else correct_output
-                    """
-                    print(f"actual: {actual_label}; detected: {detected_label}")
-                    if actual_label != detected_label:
-                        print(str(actual_label) + " " + str(detected_label) + " " + line)
-                    """
-                correct_N_total.append((correct_output, tot_input))
-            f_in.close()
-        return correct_N_total
-
-    @staticmethod
-    def window_dataset_detection(path, frames=30, tolerant=20, priority=[0, 1, 2, 1, 1]):
-        correct_N_total = []
-        for filename in os.listdir(path):
-            tot_input = 0
-            correct_output = 0
-            file_path = os.path.join(path, filename)
-            actual_label = int(filename[0])
-            with open(file_path) as f_in:
-                for line in f_in:
-                    tot_input += 1
-                    detected_label = SlidingWindow.switch_window(line.strip(), fps=frames,
-                                                                 tolerance=tolerant, priority=priority)
-                    correct_output = correct_output + 1 if actual_label == detected_label else correct_output
-                    """
-                    print(f"actual: {actual_label}; detected: {detected_label}")
-                    if actual_label != detected_label:
-                        print(str(actual_label) + " " + str(detected_label) + " " + line)
-                    """
-                correct_N_total.append((correct_output, tot_input))
-            f_in.close()
-        return correct_N_total
+def window_dataset_detection(path, frames=30, tolerant=20):
+    correct_N_total = []
+    for filename in os.listdir(path):
+        tot_input = 0
+        correct_output = 0
+        file_path = os.path.join(path, filename)
+        actual_label = int(filename[0])
+        with open(file_path) as f_in:
+            for line in f_in:
+                tot_input += 1
+                detected_label = SlidingWindow.switch_window(line.strip(), fps=frames, tolerance=tolerant)
+                correct_output = correct_output + 1 if actual_label == detected_label else correct_output
+                if actual_label != detected_label:
+                    print(f"{actual_label} {detected_label} {line}")
+            correct_N_total.append((correct_output, tot_input))
+        f_in.close()
+    return correct_N_total
 
 
 if __name__ == "__main__":
-    """
     dataset_path = "transformer_data_orig"
     frames = 30
-    tolerant = 20
-    priority = [0, 1, 2, 1, 1]
     """
     dataset_path = "transformer_data_extracted"
     frames = 3
-    tolerant = 0
-    priority = [0, 1, 2, 1, 1]
-    accuracy_curve = [0] * 60
-    for tolerant_test_num in range(60):
-        detection = SlidingWindow.window_dataset_detection(dataset_path, frames=frames,
-                                                    tolerant=tolerant_test_num, priority=priority)
+    """
+
+    """
+    accuracy_curve = [0] * 20
+    for tolerant_test_num in range(20):
+        detection = window_dataset_detection(dataset_path, frames=frames, tolerant=tolerant_test_num)
         tot_input = 0
         tot_accurate = 0
         for (i, j) in detection:
             tot_accurate += i
             tot_input += j
-        accuracy_curve[tolerant_test_num] = tot_accurate / tot_input
+        accuracy_curve[tolerant_test_num] = tot_accurate / tot_inputS
 
-    plt.plot([i for i in range(60)], accuracy_curve, label='Accuracy by Tolerance')
+    plt.plot([i for i in range(20)], accuracy_curve, label='Accuracy by Tolerance')
     plt.title('Accuracy rate')
     plt.xlabel('Tolerance')
     plt.ylabel('Accuracy')
     plt.show()
     """
-    print(SlidingWindow.switch_window("0101111111010444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444441111110011110000101000011111111111111101",
-                                      frames,tolerant,priority=priority))
-    """
+    detection = window_dataset_detection(dataset_path, frames=frames, tolerant=10)
+    tot_input = 0
+    tot_accurate = 0
+    for (i, j) in detection:
+        tot_accurate += i
+        tot_input += j
+    print(tot_accurate, tot_input)
