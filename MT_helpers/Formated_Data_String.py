be68cf6c0acf183535585f0e82c78fa1dbadb848
Minhan Tang <1900574002@qq.com>
2023-05-19 23:35:58 +1000

    Added new data

--- MT_helpers/Formated_Data_String.py
+++ MT_helpers/Formated_Data_String.py
@@ -46,6 +46,50 @@ def formatted_output(label):
     print(max_len)
 
 
+def changed_formatted_output(label):
+    f_out = open(f"data/20030519/formatted_data/{label}.in", "a")
+    max_len=0
+    # Open the file and read the lines
+    with open(f'data/20030519/orig_data/{label}.txt', 'r') as f_in:
+        lines = f_in.readlines()
+        # Convert each line to a list and join the elements
+        for line_num in range(1, len(lines), 2):
+            line = lines[line_num]
+            line_specification = lines[line_num - 1]
+            separate_point = line.index(" ")
+            fps = int(float(line[:separate_point]))
+            lst = ast.literal_eval(line[separate_point + 1:])
+            s = ''.join(str(x) for x in lst)
+
+            # write 6 fps to output file
+            s_len = len(s)
+            s_ptr = 0
+            new_s = ""
+            new_fps = 6
+            while s_ptr < s_len - fps:
+                for extracted_frames in range(1, fps // new_fps):
+                    exact_frame = s_ptr + extracted_frames * new_fps
+                    all_freq = {}
+                    for frames in s[s_ptr:exact_frame]:
+                        if frames in all_freq:
+                            all_freq[frames] += 1
+                        else:
+                            all_freq[frames] = 1
+                    res = max(all_freq, key=all_freq.get)
+                    new_s = new_s + res
+                s_ptr += fps
+            """
+            if line_specification[-8] == "1":
+                with open("data/20030519/formatted_data/0.in", "a") as f0:
+                    f0.write(new_s + "\n")
+            else:
+                f_out.write(new_s + "\n")
+            """
+
+    f_in.close()
+    f_out.close()
+    print(max_len)
+
 def categorize_by_pn(label):
     max_len=0
     # Open the file and read the lines
