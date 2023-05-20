1a8f568699dd554ba0fe2afae04a3ac5ed8f5efa
Minhan Tang <1900574002@qq.com>
2023-05-20 17:09:07 +1000

    Models!

--- MT_helpers/My_Transformer.py
+++ MT_helpers/My_Transformer.py
@@ -286,7 +286,7 @@ if __name__ == "__main__":
     batch_size = 32
     lr = 0.01
     seq_length = 120
-    num_epochs = 300
+    num_epochs = 400
 
     """
     train_dataset = TextDataset('Ten_times/')
@@ -308,9 +308,9 @@ if __name__ == "__main__":
 
     transformer = Transform(model, max_seq_length=seq_length, num_classes=num_classes)
     # transformer.load_model(model_path="Saved_Model/transformer_6fps_altered_model.pth") # transformer_3fps_1_model
-    transformer.train(dataset_path='data/20030519/formatted_data/1/', num_epochs=num_epochs)
-    transformer.save_model(model_path="Saved_Model/transformer_3fps_1_model.pth")
+    transformer.train(dataset_path='data/20030519/formatted_data/4/', num_epochs=num_epochs)
+    transformer.save_model(model_path="Saved_Model/transformer_3fps_4_model.pth")
     # transformer.save_training_loss("6fps_loss.txt")
     # transformer.plot_training_loss("6fps_loss.txt")
-    print(transformer.evaluate('data/20030519/formatted_data/1/', confusion_matrix=True))
+    print(transformer.evaluate('data/20030519/formatted_data/4/', confusion_matrix=True))
     # print(transformer.evaluate_str("000000000001100000000011111111111111111111111111111000000000000111000000000"))
