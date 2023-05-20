507d0847c3754f75ee67b7becdcf5f6d898751e2
Minhan Tang <1900574002@qq.com>
2023-05-20 15:20:42 +1000

    changed data

--- MT_helpers/My_Transformer.py
+++ MT_helpers/My_Transformer.py
@@ -279,14 +279,14 @@ class Transform:
 if __name__ == "__main__":
     vocab_size = 5
     hidden_size = 32
-    num_classes = 5
+    num_classes = 2
     num_layers = 2
     num_heads = 4
     dropout = 0.1
     batch_size = 32
     lr = 0.001
     seq_length = 120
-    num_epochs = 100
+    num_epochs = 10
 
     """
     train_dataset = TextDataset('Ten_times/')
@@ -307,10 +307,10 @@ if __name__ == "__main__":
     model.to(device)
 
     transformer = Transform(model, max_seq_length=seq_length, num_classes=num_classes)
-    # transformer.load_model(model_path="Saved_Model/transformer_6fps_altered_model.pth")
-    transformer.train(dataset_path='data/20030519/formatted_data/', num_epochs=num_epochs)
+    # transformer.load_model(model_path="Saved_Model/transformer_6fps_altered_model.pth") # transformer_3fps_1_model
+    transformer.train(dataset_path='data/20030519/formatted_data/1/', num_epochs=num_epochs)
     transformer.save_model(model_path="Saved_Model/transformer_6fps_altered_model.pth")
     # transformer.save_training_loss("6fps_loss.txt")
     # transformer.plot_training_loss("6fps_loss.txt")
-    print(transformer.evaluate('data/20230513/formatted_data/', confusion_matrix=True))
+    print(transformer.evaluate('data/20030519/formatted_data/1/', confusion_matrix=True))
     # print(transformer.evaluate_str("000000000001100000000011111111111111111111111111111000000000000111000000000"))
