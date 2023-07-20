e56cba90c62c1a8eb9a0534494ef3e18c68cfc83
Wjt1127 <1243254658@qq.com>
2023-07-20 10:54:54 +0000

    retinaface can run in HuaWei Cloud in Cpu, GPU need docker config(use docker in GPU222.20.97.88)

---
+++ Develop_Retinaface/customize_service.py
@@ -0,0 +1,231 @@
+import time
+import cv2
+import torch
+# from model_service.pytorch_model_service import PTServingBaseService
+import sys
+from pathlib import Path
+import warnings
+import os
+from pathlib import Path
+import torch
+import torch.backends.cudnn as cudnn
+import numpy as np
+from data import cfg_mnet, cfg_re50
+from layers.functions.prior_box import PriorBox
+from utils.nms.py_cpu_nms import py_cpu_nms
+import cv2
+from models.retinaface import RetinaFace
+from utils.box_utils import decode, decode_landm
+import time
+warnings.filterwarnings("ignore")
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+
+def check_keys(model, pretrained_state_dict):
+    ckpt_keys = set(pretrained_state_dict.keys())
+    model_keys = set(model.state_dict().keys())
+    used_pretrained_keys = model_keys & ckpt_keys
+    unused_pretrained_keys = ckpt_keys - model_keys
+    missing_keys = model_keys - ckpt_keys
+    print('Missing keys:{}'.format(len(missing_keys)))
+    print('Unused checkpoint keys:{}'.format(len(unused_pretrained_keys)))
+    print('Used keys:{}'.format(len(used_pretrained_keys)))
+    assert len(used_pretrained_keys) > 0, 'load NONE from pretrained checkpoint'
+    return True
+
+
+def remove_prefix(state_dict, prefix):
+    ''' Old style model is stored with all names of parameters sharing common prefix 'module.' '''
+    print('remove prefix \'{}\''.format(prefix))
+    f = lambda x: x.split(prefix, 1)[-1] if x.startswith(prefix) else x
+    return {f(key): value for key, value in state_dict.items()}
+
+
+def load_model(model, pretrained_path, load_to_cpu):
+    print('Loading pretrained model from {}'.format(pretrained_path))
+    if load_to_cpu:
+        pretrained_dict = torch.load(pretrained_path, map_location=lambda storage, loc: storage)
+    else:
+        device = torch.cuda.current_device()
+        print(device)
+        # device = 'cuda:0'
+        pretrained_dict = torch.load(pretrained_path, map_location=lambda storage, loc: storage.cuda(device))
+    if "state_dict" in pretrained_dict.keys():
+        pretrained_dict = remove_prefix(pretrained_dict['state_dict'], 'module.')
+    else:
+        pretrained_dict = remove_prefix(pretrained_dict, 'module.')
+    check_keys(model, pretrained_dict)
+    model.load_state_dict(pretrained_dict, strict=False)
+    return model
+
+def get_rotation(points):
+    """
+    Parameters
+    ----------
+    points : float32, Size = (10,)
+        coordinates of landmarks for the selected faces.
+    Returns
+    -------
+    roll    , yaw   , pitch
+    float32, float32, float32
+    """
+
+    LMx = points[0:5]  # horizontal coordinates of landmarks
+    LMy = points[5:10] # vertical coordinates of landmarks
+    
+    dPx_eyes = max((LMx[1] - LMx[0]), 1)
+    dPy_eyes = (LMy[1] - LMy[0])
+    angle = np.arctan(dPy_eyes / dPx_eyes) # angle for rotation based on slope
+    
+    alpha = np.cos(angle)
+    beta = np.sin(angle)
+    
+    print(LMx,LMy)
+    # rotated landmarks
+    LMxr = (alpha * LMx + beta * LMy + (1 - alpha) * LMx[2] / 2 - beta * LMy[2] / 2) 
+    LMyr = (-beta * LMx + alpha * LMy + beta * LMx[2] / 2 + (1 - alpha) * LMy[2] / 2)
+    
+    # average distance between eyes and mouth
+    dXtot = (LMxr[1] - LMxr[0] + LMxr[4] - LMxr[3]) / 2
+    dYtot = (LMyr[3] - LMyr[0] + LMyr[4] - LMyr[1]) / 2
+    
+    # average distance between nose and eyes
+    dXnose = (LMxr[1] - LMxr[2] + LMxr[4] - LMxr[2]) / 2
+    dYnose = (LMyr[3] - LMyr[2] + LMyr[4] - LMyr[2]) / 2
+    
+    # relative rotation 0 degree is frontal 90 degree is profile
+    Xfrontal = (-90+90 / 0.5 * dXnose / dXtot) if dXtot != 0 else 0
+    Yfrontal = (-90+90 / 0.5 * dYnose / dYtot) if dYtot != 0 else 0
+
+    return angle * 180 / np.pi, Xfrontal, Yfrontal
+
+class retinaface_model:
+    def __init__(self):
+        self.weights = ROOT / "weights/mobilenet0.25_Final.pth"
+        self.network = "mobile0.25"  # Backbone network mobile0.25 or resnet50
+        self.cpu = False  # Use cpu inference
+        self.confidence_threshold = 0.02  # confidence_threshold
+        self.top_k = 5000  # top_k
+        self.nms_threshold = 0.4  # nms_threshold
+        self.keep_top_k = 750  # keep_top_k
+        self.vis_thres = 0.8  # visualization_threshold
+        self.cfg = cfg_mnet
+        torch.set_grad_enabled(False)
+        if self.network == "mobile0.25":
+            self.cfg = cfg_mnet
+        elif self.network == "resnet50":
+            self.cfg = cfg_re50
+        
+        self.net = RetinaFace(cfg=self.cfg, phase='test')
+        self.net = load_model(self.net, self.weights, self.cpu)
+        self.net.eval()
+        print('Finished loading model!')
+        # print(net)
+        
+    def inference(self, source):
+        tic = time.time()
+        self.video_path = ROOT / str(source)
+        cudnn.benchmark = True
+        if self.cpu:
+            device = torch.device("cpu")
+        else:
+            device = torch.cuda.current_device()
+        print(device)
+        self.net = self.net.to(device)
+        resize = 1
+
+        image_path = str(self.video_path)
+        img_raw = cv2.imread(image_path, cv2.IMREAD_COLOR)
+
+        img = np.float32(img_raw)
+
+        im_height, im_width, _ = img.shape
+        scale = torch.Tensor([img.shape[1], img.shape[0], img.shape[1], img.shape[0]])
+        img -= (104, 117, 123)
+        img = img.transpose(2, 0, 1)
+        img = torch.from_numpy(img).unsqueeze(0)
+        img = img.to(device)
+        scale = scale.to(device)
+
+        # print('input shape ', img.shape)
+        # tic = time.time()
+        loc, conf, landms = self.net(img)  # forward pass
+        print('net forward time: {:.4f}'.format(time.time() - tic))
+
+        priorbox = PriorBox(self.cfg, image_size=(im_height, im_width))
+        priors = priorbox.forward()
+        priors = priors.to(device)
+        prior_data = priors.data
+        boxes = decode(loc.data.squeeze(0), prior_data, self.cfg['variance'])
+        boxes = boxes * scale / resize
+        boxes = boxes.cpu().numpy()
+        scores = conf.squeeze(0).data.cpu().numpy()[:, 1]
+        landms = decode_landm(landms.data.squeeze(0), prior_data, self.cfg['variance'])
+        scale1 = torch.Tensor([img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                               img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                               img.shape[3], img.shape[2]])
+        scale1 = scale1.to(device)
+        landms = landms * scale1 / resize
+        landms = landms.cpu().numpy()
+
+        # ignore low scores
+        inds = np.where(scores > self.confidence_threshold)[0]
+        boxes = boxes[inds]
+        landms = landms[inds]
+        scores = scores[inds]
+
+        # keep top-K before NMS
+        order = scores.argsort()[::-1][:self.top_k]
+        boxes = boxes[order]
+        landms = landms[order]
+        scores = scores[order]
+
+        # do NMS
+        dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
+        keep = py_cpu_nms(dets, self.nms_threshold)
+        # keep = nms(dets, nms_threshold,force_cpu=cpu)
+        dets = dets[keep, :]
+        landms = landms[keep]
+
+        # keep top-K faster NMS
+        dets = dets[:self.keep_top_k, :]
+        landms = landms[:self.keep_top_k, :]
+
+        dets = np.concatenate((dets, landms), axis=1)
+        print(dets)
+        for b in dets:
+            if b[4] < self.vis_thres:
+                continue
+
+            bb = np.array(b)
+
+            # x\y
+            point = []
+            for i in range(5, 15):
+                if i % 2 == 0:
+                    point.append(bb[i])  # point[0/2/4/6/8] is x
+
+            for i in range(5, 15):
+                if i % 2 == 1:
+                    point.append(bb[i])  # point[1/3/5/7/9] is y
+
+            point = np.transpose(point)
+            angle, Xfrontal, Yfrontal = get_rotation(point)
+
+            print("Roll: {0:.2f} degrees".format(angle))
+            print("Yaw: {0:.2f} degrees".format(Xfrontal))
+            print("Pitch: {0:.2f} degrees".format(Yfrontal))
+
+            result = {"result": {"Roll": 0.0, "Yaw": 0.0, "Pitch": 0.0, "duration": 6000}}
+
+            result['result']['Roll'] = angle
+            result['result']['Yaw'] = Xfrontal
+            result['result']['Pitch'] = Yfrontal
+            result['result']['duration'] = int((time.time() - tic) * 1000)
+        
+        return result
---
+++ Develop_Retinaface/rotation.py
@@ -0,0 +1,288 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+
+# load retinaFace face detector
+detector_model = tf.saved_model.load('./models/tf_retinaface_mbv2/')
+
+def one_face(frame, bbs, pointss):
+    """
+    Parameters
+    ----------
+    frame : uint8
+        RGB image (numpy array).
+    bbs : float64, Size = (N, 4)
+        coordinates of bounding boxes for all detected faces.
+    pointss : flaot32, Size = (N, 10)
+        coordinates of landmarks for all detected faces.
+
+    Returns
+    -------
+    bb : float64, Size = (5,)
+        coordinates of bounding box for the selected face.
+    points : float32
+        coordinates of five landmarks for the selected face.
+
+    """
+    # select only process only one face (center ?)
+    offsets = [(bbs[:,0]+bbs[:,2])/2-frame.shape[1]/2,
+               (bbs[:,1]+bbs[:,3])/2-frame.shape[0]/2]
+    offset_dist = np.sum(np.abs(offsets),0)
+    index = np.argmin(offset_dist)
+    bb = bbs[index]
+    points = pointss[:,index]
+    return bb, points
+
+def are_coordinates_in_frame(frame, box, pts):
+    """
+    Parameters
+    ----------
+    frame : uint8
+        RGB image (numpy array).
+    bbs : float64
+        coordinates of bounding box.
+    points : flaot32
+        coordinates of landmarks.
+
+    Returns
+    -------
+    boolean
+    """
+    
+    height, width = frame.shape[:2]
+    
+    if np.any(box <= 0) or np.any(box >= height) or np.any(box >= width):
+        return False
+    if np.any(pts <= 0) or np.any(pts >= height) or np.any(pts >= width):
+        return False
+    
+    return True
+    
+            
+def draw_landmarks(frame, bb, points):
+    '''
+    Parameters
+    ----------
+    frame : uint8
+        RGB image
+    bb : float64, Size = (5,)
+        coordinates of bounding box for the selected face.
+    points : float32, Size = (10,)
+        coordinates of landmarks for the selected faces.
+
+    Returns
+    -------
+    None.
+
+    '''
+    bb = bb.astype(int)
+    points = points.astype(int)
+    # draw rectangle and landmarks on face
+    cv2.rectangle(frame, (bb[0], bb[1]), (bb[2], bb[3]), red, 1)
+    cv2.circle(frame, (points[0], points[5]), 2, blue, 2)# left eye
+    cv2.circle(frame, (points[1], points[6]), 2, blue, 2)# right eye
+    cv2.circle(frame, (points[2], points[7]), 2, blue, 2)# nose
+    cv2.circle(frame, (points[3], points[8]), 2, blue, 2)# mouth - left
+    cv2.circle(frame, (points[4], points[9]), 2, blue, 2)# mouth - right 
+    
+    w = int(bb[2])-int(bb[0])# width
+    h = int(bb[3])-int(bb[1])# height
+    w2h_ratio = w/h# width to height ratio
+    eye2box_ratio = (points[0]-bb[0]) / (bb[2]-points[1])
+
+    #cv2.putText(frame, "Width (pixels): {}".format(w), (10,30), font, font_size, red, 1)
+    #cv2.putText(frame, "Height (pixels): {}".format(h), (10,40), font, font_size, red, 1)
+    
+    if eye2box_ratio > 1.5 or eye2box_ratio < 0.88:
+        cv2.putText(frame, "Face: not in center of the bounding box", (10, 140), font, 0.4, blue, 1)
+    if w2h_ratio < 0.7 or w2h_ratio > 0.9:
+        cv2.putText(frame, "Face: long and narrow", (10, 160), font, 0.4, blue, 1)
+
+def find_pose(points):
+    """
+    Parameters
+    ----------
+    points : float32, Size = (10,)
+        coordinates of landmarks for the selected faces.
+    Returns
+    -------
+    roll    , yaw   , pitch
+    float32, float32, float32
+    """
+    LMx = points[0:5]  # horizontal coordinates of landmarks
+    LMy = points[5:10] # vertical coordinates of landmarks
+    
+    dPx_eyes = max((LMx[1] - LMx[0]), 1)
+    dPy_eyes = (LMy[1] - LMy[0])
+    angle = np.arctan(dPy_eyes / dPx_eyes) # angle for rotation based on slope
+    
+    alpha = np.cos(angle)
+    beta = np.sin(angle)
+    
+    # rotated landmarks
+    LMxr = (alpha * LMx + beta * LMy + (1 - alpha) * LMx[2] / 2 - beta * LMy[2] / 2) 
+    LMyr = (-beta * LMx + alpha * LMy + beta * LMx[2] / 2 + (1 - alpha) * LMy[2] / 2)
+    print(LMx,LMy)
+    # average distance between eyes and mouth
+    dXtot = (LMxr[1] - LMxr[0] + LMxr[4] - LMxr[3]) / 2
+    dYtot = (LMyr[3] - LMyr[0] + LMyr[4] - LMyr[1]) / 2
+    
+    # average distance between nose and eyes
+    dXnose = (LMxr[1] - LMxr[2] + LMxr[4] - LMxr[2]) / 2
+    dYnose = (LMyr[3] - LMyr[2] + LMyr[4] - LMyr[2]) / 2
+    
+    # relative rotation 0 degree is frontal 90 degree is profile
+    Xfrontal = (-90+90 / 0.5 * dXnose / dXtot) if dXtot != 0 else 0
+    Yfrontal = (-90+90 / 0.5 * dYnose / dYtot) if dYtot != 0 else 0
+
+    return angle * 180 / np.pi, Xfrontal, Yfrontal
+
+def detect_faces(image, image_shape_max=640):
+    '''
+    Performs face detection using retinaface method with speed boost and 
+    initial quality checks based on whole image size
+    
+    Parameters
+    ----------
+    image : uint8
+        image for face detection.
+    image_shape_max : int, optional
+        maximum size (in pixels) of image.
+
+    Returns
+    -------
+    float array
+        landmarks.
+    float array
+        bounding boxes.
+    flaot array
+        detection scores.
+    '''
+
+    image_shape = image.shape[:2]
+    
+    # perform image resize for faster detection    
+    if image_shape_max:
+        scale_factor = max([1, max(image_shape)/image_shape_max])
+    else:
+        scale_factor = 1
+
+    if scale_factor > 1:        
+        scaled_image = cv2.resize(image, (0, 0), fx = 1 / scale_factor, 
+                                  fy = 1 / scale_factor)
+        bbs_all, points_all = retinaface(scaled_image)
+        bbs_all[:,:4] *= scale_factor
+        points_all *= scale_factor
+    else:
+        bbs_all, points_all = retinaface(image)              
+
+    scores = bbs_all[:,-1]
+    bbs = bbs_all[:, :4]
+
+    return points_all, bbs, scores
+
+def retinaface(image):
+    """ retinaface face detector"""
+
+    height = image.shape[0]
+    width = image.shape[1]
+    
+    image_pad, pad_params = pad_input_image(image)    
+    image_pad = tf.convert_to_tensor(image_pad[np.newaxis, ...])
+    image_pad = tf.cast(image_pad, tf.float32)  
+   
+    outputs = detector_model(image_pad).numpy()
+
+    outputs = recover_pad_output(outputs, pad_params)
+    Nfaces = len(outputs)
+    
+    bbs = np.zeros((Nfaces,5))
+    lms = np.zeros((Nfaces,10))
+    
+    bbs[:,[0,2]] = outputs[:,[0,2]]*width
+    bbs[:,[1,3]] = outputs[:,[1,3]]*height
+    bbs[:,4] = outputs[:,-1]
+    
+    lms[:,0:5] = outputs[:,[4,6,8,10,12]]*width
+    lms[:,5:10] = outputs[:,[5,7,9,11,13]]*height
+    
+    return bbs, lms
+
+def pad_input_image(img, max_steps=32):
+    """pad image to suitable shape - required for retinaface"""
+    img_h, img_w, _ = img.shape
+
+    img_pad_h = 0
+    if img_h % max_steps > 0:
+        img_pad_h = max_steps - img_h % max_steps
+
+    img_pad_w = 0
+    if img_w % max_steps > 0:
+        img_pad_w = max_steps - img_w % max_steps
+
+    padd_val = np.mean(img, axis=(0, 1)).astype(np.uint8)
+    img = cv2.copyMakeBorder(img, 0, img_pad_h, 0, img_pad_w,
+                             cv2.BORDER_CONSTANT, value=padd_val.tolist())
+    pad_params = (img_h, img_w, img_pad_h, img_pad_w)
+
+    return img, pad_params
+
+def recover_pad_output(outputs, pad_params):
+    """recover the padded output effect"""
+    img_h, img_w, img_pad_h, img_pad_w = pad_params
+    recover_xy = np.reshape(outputs[:, :14], [-1, 7, 2]) * \
+        [(img_pad_w + img_w) / img_w, (img_pad_h + img_h) / img_h]
+    outputs[:, :14] = np.reshape(recover_xy, [-1, 14])
+
+    return outputs
+
+if __name__ == '__main__':
+    #============================================================================
+    # FONT SETTING (font style, size and color)
+    font = cv2.FONT_HERSHEY_COMPLEX # Text in video
+    blue = (0, 0, 255)
+    green = (0,128,0)
+    red = (255, 0, 0)
+
+    image_path = "../Pytorch_Retinaface/curve/1.jpg"
+    image_rgb = cv2.imread(image_path, cv2.COLOR_BGR2RGB)
+    image_rgb = cv2.flip(image_rgb, 1)# flip for user friendliness
+
+    # face detection
+    try:
+        landmarks, bboxes, scores = detect_faces(image_rgb,image_shape_max=640)
+    except:
+        print("Error: face detector error.")
+
+    # if at least one face is detected
+    if len(bboxes) > 0:
+        
+        #bboxes = np.insert(bboxes,bboxes.shape[1], scores, axis=1)
+        lmarks = np.transpose(landmarks)
+        bbs = bboxes.copy()
+        print(bbs, lmarks)
+        # process only one face (center ?) if multiple faces detected  
+        bb, lmarks_5 = one_face(image_rgb, bbs, lmarks)
+        print(bb,landmarks)
+        # if are_coordinates_in_frame(image_rgb, bb, lmarks_5):
+        if True:        
+            draw_landmarks(image_rgb, bb, lmarks_5)# draw landmarks and bbox   
+
+            angle, Xfrontal, Yfrontal = find_pose(lmarks_5)
+            # cv2.putText(image_rgb, "Method 2", (10, 180), font, 0.4, blue, 2)
+            # cv2.putText(image_rgb, "Roll: {0:.2f} degrees".format(angle), 
+            #             (10,200), font, 0.4, blue, 1)
+            # cv2.putText(image_rgb, "Yaw: {0:.2f} degrees".format(Xfrontal), 
+            #             (10,220), font, 0.4, blue, 1)
+            # cv2.putText(image_rgb, "Pitch: {0:.2f} degrees".format(Yfrontal), 
+            #             (10,240), font, 0.4, blue, 1)
+            
+            print( "Roll: {0:.2f} degrees".format(angle))
+            print("Yaw: {0:.2f} degrees".format(Xfrontal))
+            print("Pitch: {0:.2f} degrees".format(Yfrontal))
+
+    else:
+        cv2.putText(image_rgb, 'no face detected', (10,20), font, 0.4, blue, 2)
+
+    name = "test.jpg"
+    cv2.imwrite(name, image_rgb)
