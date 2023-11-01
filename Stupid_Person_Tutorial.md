340fba78b84c81b5f574f0de47049d43f85f5d7d
Nobody-Zhang <minecraftzhanggongbo@outlook.com>
2023-11-01 14:20:24 +0800

    比赛代码整合缩减

--- Stupid_Person_Tutorial.md
+++
@@ -1,7 +0,0 @@
-# Tutorial
-#### From an outsider
-## Prepare Environments
-1. Install Anaconda and PyCharm(don't know why, just do it)
-2. Create and activate a virtual environment
-3. Install pytorch with cuda first following https://pytorch.org/get-started/locally/
-4. Install what's on the requirements.txt
--- all_generate_data.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-file = r'D:\Workspace\github\yolov5\train-data'
-for root, dirs, files in os.walk(file):
-    for file in files:
-        path = os.path.join(root, file)
-        t1 = time.time()
-        print(path) #当前的绝对路径
-        os.system("python mixed.py --weights best1.pt --source %s --nosave" % path)
